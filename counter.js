5b8f2297aa3404129bba98dd7b18f68a3b08d399
hirouval <ftisland1218@icloud.com>
2026-05-05 22:41:59 +0900

    add js all

--- counter.js
+++ counter.js
@@ -1,3 +1,18 @@
-const counter () => {
+const counter = () => {
+    const $counter = document.getElementById("js-counter");
     
+    
+    const clickHandler = (e) => {
+        const $targetButton = e.currentTarget;
+        let currentCount = parseInt($counter.textContent);
+        if($targetButton.textContent === "+"){
+            $counter.textContent = currentCount + 1;
+        } else {
+            $counter.textContent = currentCount - 1;
+        }
+    }
+    for (let index = 0; index < document.getElementsByClassName("js-button").length; index++){
+        ducument.getElementsByClassName("js-button")[index].
+        addEventListener("click", (e) => clickHandler(e))
+    }
 }
